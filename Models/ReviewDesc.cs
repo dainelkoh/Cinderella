9dd70d999115517ae51d456c55a69e7cd62f5196
arcane-cs <ngwaikeet@gmail.com>
2020-07-28 22:21:36 +0800

    Added Review and ReviewDesc class

---
+++ Models/ReviewDesc.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cinderella.Models
+{
+    public class ReviewDesc
+    {
+        public int ReviewDescID { get; set; }
+
+        [ForeignKey("Review")]
+        public int ReviewID { get; set; }
+        public string ReviewName { get; set; }
+        public string ReviewWords { get; set; }
+    }
+}
