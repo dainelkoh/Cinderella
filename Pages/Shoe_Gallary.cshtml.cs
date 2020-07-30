8e9c72d3e282902874ed97d6c7d82eb879c28165
Zuriel <zuriel.shanley@gmail.com>
2020-07-31 01:04:15 +0800

    Added Gallery tab, about us and add more pictures to database

---
+++ Pages/Shoe_Gallary.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Cinderella.Pages
+{
+    public class Shoe_GallaryModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
